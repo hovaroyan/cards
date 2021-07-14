8d9e27405daad9339ba7c21ab9482dbbe97c2b01
hovaroyan <hovaroyan@gmail.com>
2021-07-14 13:20:24 +0400

    added a todo-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoCardComponent } from './todo-card/todo-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo-card/todo-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-card',
+  templateUrl: './todo-card.component.html',
+  styleUrls: ['./todo-card.component.css']
+})
+export class TodoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
