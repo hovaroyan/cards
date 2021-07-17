2c95b2a19f0dbdcf431e18bd01cfe4d48d42173f
hovaroyan <hovaroyan@gmail.com>
2021-07-17 04:03:49 +0400

    design fix

--- src/app/todo-card/cards.ts
+++ src/app/todo-card/cards.ts
@@ -1,19 +1,19 @@
 export const CARDS =[
     {
         image: 'assets/travel.jpeg',
-        title: 'traveling',
+        title: 'Traveling',
         description: 'travel to Canada to see friends',
         dueDate: 'september 8th',
     },
     {   
         image: 'assets/concert.jpeg',
-        title: 'attending an event',
+        title: 'Attending an event',
         description: 'going to a music concert',
         dueDate: 'october 17th',
     },
     {
         image: 'assets/wedding.jpeg',
-        title: 'party',
+        title: 'Party',
         description: `attending a friend's wedding party`,
         dueDate: 'october 25th',
     },
