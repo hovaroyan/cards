bdec7ddd65cfc0f0347fdba2b7637b5809a035fa
hovaroyan <hovaroyan@gmail.com>
2021-07-18 18:51:24 +0400

    fixed button functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,35 @@ import { CARDS } from './todo-card/cards';
 export class AppComponent {
   cards = CARDS;
   cardsCount: number = 0;
+  addDisable: boolean = false;
+  deleteDisable: boolean = false;
+  resetDisable: boolean = false;
+
 
   handleAdd() {
     this.cardsCount +=1;
+    this.deleteDisable = true;
+    this.resetDisable = true;
+    if(this.cardsCount === 10) {
+      this.addDisable = true;
+    }
+    console.log(this.cardsCount);
   }
 
   handleDelete() {
     this.cardsCount -=1;
+    if(this.cardsCount <=0) {
+      this.deleteDisable = false;
+    } 
+    console.log(this.cardsCount);
   }
 
   handleReset() {
     this.cardsCount -= this.cardsCount;
+    if(this.cardsCount <=0) {
+      this.resetDisable = false;
+    }
+    console.log(this.cardsCount);
   }
 };
 
